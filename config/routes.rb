ab0667ba5a50f022a78058740285e40d9aaed5b0
Graeme Harrison <gharrison365@gmail.com>
2020-09-12 11:52:02 -0400

    Add root to home#index to routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/front'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Possibly need for Devise - Graeme
+  root to: "home#index"
 end
