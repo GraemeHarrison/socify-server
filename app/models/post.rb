9a77258973c5fdd5eb0ca64e131c271e5a4535bf
Graeme Harrison <gharrison365@gmail.com>
2020-09-13 10:41:51 -0400

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+  acts_as_votable
+  acts_as_commentable
 
   include PublicActivity::Model
   tracked only: [:create], owner: Proc.new{ |controller, model| controller.current_user }
 
   mount_uploader :attachment, AvatarUploader
+
+  validates_presence_of :content
+  validates_presence_of :user
 end
